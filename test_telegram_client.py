f4afd55cdc811a46f475285509a8f24130ae86cb
danielwangai <mainad57@gmail.com>
2017-01-18 18:24:27 +0300

    Add test to check for wrong chat_id

--- test_telegram_client.py
+++ test_telegram_client.py
@@ -4,12 +4,15 @@ import telegram_client
 class TelegramBotTests(unittest.TestCase):
     def setUp(self):
         self.correct_chat_id = 173669942
+        self.incorrect_correct_chat_id = 12345
 
     def test_sends_message_successfully(self):
         # response after posting to telegram API
         response = telegram_client.send_message(self.correct_chat_id)
         # get a list of values(from dictionary) and gets the first - check of ok is true
         self.assertTrue(list(response.values())[0])
+    def test_wrong_chat_id(self):
+        self.assertEqual(telegram_client.send_message(self.incorrect_correct_chat_id)['error_code'], 400)
 
 if __name__== '__main__':
     unittest.main()
