b756e6e872d679103e3ed22a3c22cc63a4a7d3a4
danielwangai <mainad57@gmail.com>
2017-01-18 19:37:13 +0300

    Add function to get updates from bot

--- telegram_client.py
+++ telegram_client.py
@@ -7,3 +7,7 @@ def send_message(chat_id):
    action = "sendMessage"
    response = requests.post(url+telegram_token+"/"+action, {'chat_id': chat_id, 'text': 'Test'}).json()
    return response
+
+def get_updates():
+    response = requests.get(url+telegram_token+'/'+'getUpdates').json()
+    return response
