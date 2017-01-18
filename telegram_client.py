727c47450dcfc8e708ffb6c293f6d49af2d1d510
danielwangai <mainad57@gmail.com>
2017-01-18 19:36:48 +0300

    Add method for sending message to user on telegram.

---
+++ telegram_client.py
@@ -0,0 +1,9 @@
+import requests
+
+telegram_token = "295227945:AAH8aElKnUzIitrlvuvD-RzkRz31U1-0HXw"
+url = "https://api.telegram.org/bot"
+
+def send_message(chat_id):
+   action = "sendMessage"
+   response = requests.post(url+telegram_token+"/"+action, {'chat_id': chat_id, 'text': 'Test'}).json()
+   return response
