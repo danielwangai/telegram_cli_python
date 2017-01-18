9121ca2d2384a77a4cd67770831b9d1ed8ae5862
danielwangai <mainad57@gmail.com>
2017-01-18 18:13:34 +0300

    Add setUp method containing variables for tests

--- test_telegram_client.py
+++ test_telegram_client.py
@@ -2,9 +2,12 @@ import unittest
 import telegram_client
 
 class TelegramBotTests(unittest.TestCase):
+    def setUp(self):
+        self.correct_chat_id = 173669942
+
     def test_sends_message_successfully(self):
         # response after posting to telegram API
-        response = telegram_client.send_message(173669942)
+        response = telegram_client.send_message(self.correct_chat_id)
         # get a list of values(from dictionary) and gets the first - check of ok is true
         self.assertTrue(list(response.values())[0])
 
