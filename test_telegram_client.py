b2c1d4ca8aff94836dc7ecfb00c146fd0c368ef9
danielwangai <mainad57@gmail.com>
2017-01-18 18:56:26 +0300

    Add test to check that bot token is correct

--- test_telegram_client.py
+++ test_telegram_client.py
@@ -1,10 +1,14 @@
 import unittest
+import requests
 import telegram_client
 
 class TelegramBotTests(unittest.TestCase):
     def setUp(self):
         self.correct_chat_id = 173669942
         self.incorrect_correct_chat_id = 12345
+        self.correct_telegram_token = "295227945:AAH8aElKnUzIitrlvuvD-RzkRz31U1-0HXw"
+        self.wrong_telegram_token = "123446789:ANT8aElKnUzIitrlvuvD-RzkRz31U1-0HXw"
+        self.url = "https://api.telegram.org/bot"
 
     def test_sends_message_successfully(self):
         # response after posting to telegram API
@@ -13,6 +17,10 @@ class TelegramBotTests(unittest.TestCase):
         self.assertTrue(list(response.values())[0])
     def test_wrong_chat_id(self):
         self.assertEqual(telegram_client.send_message(self.incorrect_correct_chat_id)['error_code'], 400)
+
+    def test_that_bot_token_is_correct(self):
+        response = requests.get(self.url+self.correct_telegram_token+'/'+'getme').json()
+        self.assertTrue(response['ok'])
 
 if __name__== '__main__':
     unittest.main()
