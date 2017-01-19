95c897d1a1ce5ee58d654916283084a52ae89c0e
danielwangai <mainad57@gmail.com>
2017-01-19 22:45:56 +0300

    Add user input to send_message

--- telegram_client.py
+++ telegram_client.py
@@ -4,10 +4,14 @@ telegram_token = "295227945:AAH8aElKnUzIitrlvuvD-RzkRz31U1-0HXw"
 url = "https://api.telegram.org/bot"
 
 def send_message(chat_id):
-   action = "sendMessage"
-   response = requests.post(url+telegram_token+"/"+action, {'chat_id': chat_id, 'text': 'Test'}).json()
-   return response
+    action = "sendMessage"
+    user_message = input("Enter message to be sent:-\n")
+    response = requests.post(url+telegram_token+"/"+action, {'chat_id': chat_id, 'text': user_message}).json()
+    return response
 
 def get_updates():
     response = requests.get(url+telegram_token+'/'+'getUpdates').json()
     return response
+
+send_message(173669942)
+# usr = raw_input("Enter name")
