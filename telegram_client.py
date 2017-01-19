b3662bc55c1bd9fae398c5d9eec7c1651ea9f990
danielwangai <mainad57@gmail.com>
2017-01-19 23:30:12 +0300

    Put checks in function returning 1 for success and 0 for empty command, 1 for success and 2 for unrecognized command

--- telegram_client.py
+++ telegram_client.py
@@ -14,14 +14,20 @@ def get_updates():
     return response
 
 system_commands = ['/update']
-user_input = input("Enter command")
-if user_input != "":
-    if user_input in system_commands:
-        if user_input == "/update":
-            first_name = get_updates()['result'][0]['message']['chat']['first_name']
-            last_name = get_updates()['result'][0]['message']['chat']['last_name']
-            print("A new user "+first_name+" "+last_name+" used the bot.")
+def get_user_commands():
+    user_input = input("Enter command")
+    if user_input != "":
+        if user_input in system_commands:
+            if user_input == "/update":
+                first_name = get_updates()['result'][0]['message']['chat']['first_name']
+                last_name = get_updates()['result'][0]['message']['chat']['last_name']
+                print("A new user "+first_name+" "+last_name+" used the bot.")
+                return 1
+        else:
+            print("command not recognized")
+            return 2
     else:
-        print("command not recognized")
-else:
-    print("Input cannot be empty. Try Again.")
+        print("Input cannot be empty. Try Again.")
+        return 0
+
+get_user_commands()
