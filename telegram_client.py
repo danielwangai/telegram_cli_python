09172ed4239c77ddb792387c2963da1b11c68ee1
danielwangai <mainad57@gmail.com>
2017-01-19 23:59:06 +0300

    Add new argument to send_message

--- telegram_client.py
+++ telegram_client.py
@@ -3,10 +3,9 @@ import requests
 telegram_token = "295227945:AAH8aElKnUzIitrlvuvD-RzkRz31U1-0HXw"
 url = "https://api.telegram.org/bot"
 
-def send_message(chat_id):
+def send_message(chat_id, message_from_user):
     action = "sendMessage"
-    user_message = input("Enter message to be sent:-\n")
-    response = requests.post(url+telegram_token+"/"+action, {'chat_id': chat_id, 'text': user_message}).json()
+    response = requests.post(url+telegram_token+"/"+action, {'chat_id': chat_id, 'text': message_from_user}).json()
     return response
 
 def get_updates():
@@ -14,11 +13,11 @@ def get_updates():
     return response
 
 system_commands = ['/update']
-def get_user_commands():
-    user_input = input("Enter command")
-    if user_input != "":
-        if user_input in system_commands:
-            if user_input == "/update":
+
+def get_user_commands(input_from_user):
+    if input_from_user != "":
+        if input_from_user in system_commands:
+            if input_from_user == "/update":
                 first_name = get_updates()['result'][0]['message']['chat']['first_name']
                 last_name = get_updates()['result'][0]['message']['chat']['last_name']
                 print("A new user "+first_name+" "+last_name+" used the bot.")
@@ -27,7 +26,17 @@ def get_user_commands():
             print("command not recognized")
             return 2
     else:
-        print("Input cannot be empty. Try Again.")
+        print("Input cannot be empty.")
         return 0
+#
+# def send_message_from_cli(message):
+#     if message == "":
+#         print("Message cannot be empty!")
+#         return 0
+#     else:
+#         if send_message():
+#             pass
 
-get_user_commands()
+user_message = input("Enter message to be sent:-\n")
+user_input = input("Enter command")
+get_user_commands(user_input)
